c2a2eb05bf6b70d338022ada72223576fd141769
Nicola <ziapo98@gmail.com>
2022-08-24 13:36:28 +0200

    Step response works by removing integral action in human intention controller, displacement remains

--- hw6/force_velocity/hw6_main_fv.m
+++ hw6/force_velocity/hw6_main_fv.m
@@ -1,16 +1,16 @@
 %% scattering based force velocity
 
 %Sin frequency and amplitude of the reference signal
-Fc = 0.1;
-Amp = 0.8; %0.8
+Fc_sin = 0.1;
+Amp = 0.3; %0.8
 
 delay = 10;
 
 % cutoff frequency of the wave transformation filter
-F_wave = 8;
+Fc_wave = 8;
 
 %Frequency of the low pass filter
-Fip = 5;
+Fc_lp = 5;
 
 %Human Controller/Human intention
 Dh = 8;
@@ -23,7 +23,7 @@ Ks = 80;
 % Human Impedance
 Jh = 0;  
 Bh = 1.5;  
-Kh = 1; %1.0; 
+Kh = 0.1; %1.0; 
 
 %Environment Impedance
 Je = 0;
