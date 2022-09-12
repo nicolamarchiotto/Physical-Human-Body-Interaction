3ada719c76ac4428ab651d810e9504bbfbbad84e
Nicola <ziapo98@gmail.com>
2022-09-12 11:45:17 +0200

    edit doc

--- hw5/RLS.m
+++ hw5/RLS.m
@@ -1,15 +1,28 @@
 function [Y_hat_rls,k_rls,tau_rls]=RLS(X,Y,P0,lamba)
+% RLS, the lamba must be in the range 0.005 < lamba < 1 for computational
+% reasons, if lamba < 0.005, lamba is set to 0.005
+
 rls=[];
 b0=[0;0];
 for k=1:size(X,1)
     if(k==1)
         rls(k).e=Y(k,:)-X(k,:)*b0;
-        rls(k).P=(P0-(P0*X(k,:)'*X(k,:)*P0)/(lamba+X(k,:)*P0*X(k,:)')/lamba);
+        if lamba >= 0.005
+            rls(k).P=(P0-(P0*X(k,:)'*X(k,:)*P0)/(lamba+X(k,:)*P0*X(k,:)')/lamba);
+        else
+            lamba_temp=0.005;
+            rls(k).P=(P0-(P0*X(k,:)'*X(k,:)*P0)/(lamba_temp+X(k,:)*P0*X(k,:)')/lamba_temp);
+        end
         rls(k).K=rls(k).P*X(k,:)';
         rls(k).b=b0+rls(k).K*rls(k).e;
     else
         rls(k).e=Y(k,:)-X(k,:)*rls(k-1).b;
-        rls(k).P=(rls(k-1).P-(rls(k-1).P*X(k,:)'*X(k,:)*rls(k-1).P)/(lamba+X(k,:)*rls(k-1).P*X(k,:)')/lamba);
+        if lamba > 0.005
+            rls(k).P=(rls(k-1).P-(rls(k-1).P*X(k,:)'*X(k,:)*rls(k-1).P)/(lamba+X(k,:)*rls(k-1).P*X(k,:)')/lamba); 
+        else
+            lamba_temp=0.005;
+            rls(k).P=(rls(k-1).P-(rls(k-1).P*X(k,:)'*X(k,:)*rls(k-1).P)/(lamba_temp+X(k,:)*rls(k-1).P*X(k,:)')/lamba_temp); 
+        end
         rls(k).K=rls(k).P*X(k,:)';
         rls(k).b=rls(k-1).b+rls(k).K*rls(k).e;
     end
--- hw5/hw5_main.m
+++ hw5/hw5_main.m
@@ -23,39 +23,43 @@ Y_hat_ls=X*b_hat_ls;
 
 k_ls=1/b_hat_ls(2);
 tau_ls=b_hat_ls(1)*k_ls;
+k_ls
+tau_ls
 
-% RLS
+% RLS, the lamba must be in the range 0.005 < lamba < 1 for computational
+% reasons, if lamba < 0.005, lamba is set to 0.005
 
 P0=diag([1 1]);
 lamba=1;
 [Y_hat_rls,k_rls,tau_rls]=RLS(X,Y,P0,lamba);
-
+k_rls
+tau_rls
 % Adaptive algorithm
 
 g=0.3;
 [Y_hat_adpt, k_adpt, tau_adpt]=adativeAlgorithm(X,Y,Ts,g);
 
 % Data vs LS
-figure;
-plot(t,Y);
-hold on;
-plot(t,Y_hat_ls);
-legend('Motors data','LS');
-xlabel('Time')
-ylabel('Voltage')
-title("LS: Parameters: k "+k_ls+" tau "+tau_ls)
-
-% Data vs LS vs RLS
-
-figure;
-plot(t,Y);
-hold on;
-plot(t,Y_hat_ls);
-plot(t,Y_hat_rls);
-legend('Motors data','LS', 'RLS');
-xlabel('Time')
-ylabel('Voltage')
-title("RLS: Final parameters: k "+k_rls+" tau "+tau_rls)
+% figure;
+% plot(t,Y);
+% hold on;
+% plot(t,Y_hat_ls);
+% legend('Motors data','LS');
+% xlabel('Time')
+% ylabel('Voltage')
+% title("LS: Parameters: k "+k_ls+" tau "+tau_ls)
+% 
+% % Data vs LS vs RLS
+% 
+% figure;
+% plot(t,Y);
+% hold on;
+% plot(t,Y_hat_ls);
+% plot(t,Y_hat_rls);
+% legend('Motors data','LS', 'RLS');
+% xlabel('Time')
+% ylabel('Voltage')
+% title("RLS: Final parameters: k "+k_rls+" tau "+tau_rls)
 
 % Data vs LS vs RLS vs Adaptive Algorithm
 
